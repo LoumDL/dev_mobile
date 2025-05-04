9a4ad280d90b13638d74bd655f8c8919d561bfe4
= <=>
2025-05-04 22:27:09 +0000

    fin provisoire de la version2

--- todolist_version2/www/js/index.js
+++ todolist_version2/www/js/index.js
@@ -3,6 +3,7 @@ window.onload = function() {
     const resetButton = document.getElementById("resetButton");
     const taskField = document.getElementById("taskField");
     const tasklist = document.getElementById("tasklist");
+    const tasktermine = document.getElementById("tasktermine");
 
     addButton.onclick = function() {
         if (taskField.value) {
@@ -12,21 +13,34 @@ window.onload = function() {
             $(newItem).on('swiperight', function() {
                 newItem.classList.toggle("done");
             });
+
             $(newItem).on('swipeleft', function() {
-                $(newItem).hide('slow',function() {
+                
+                /* $(newItem).hide('slow',function() {
                     $newItem.remove();
+                    //tasktermine.appendChild(newItem);
+                   
                 }
-                );
+                )*/
+               tasktermine.appendChild(newItem)
             });
 
             tasklist.appendChild(newItem);
             $(tasklist).listview("refresh");
             $(taskField).select();
+
+            /* $(newItem).on('swipelet',function(){
+                $(newItem).hide('slow',function(){
+                    tasktermine.appendChild(newItem)
+                })
+
+            }); */
         }
     };
 
     resetButton.onclick = function() {
         tasklist.innerHTML = "";
+        tasktermine.innerHTML="";
         taskField.value = "";
         $(taskField).focus();
     };
